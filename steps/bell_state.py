b784da65b3c408c1fd1363536139caaa3ec9a6d7
Masaki Ikeda <masaki.ikeda@zapatacomputing.com>
2021-05-07 02:06:07 +0900

    Modified for Training

--- steps/bell_state.py
+++ steps/bell_state.py
@@ -4,12 +4,13 @@ import json
 
 def main():
     # Define registers and circuit
-    q = QuantumRegister(1)
-    c = ClassicalRegister(1)
+    q = QuantumRegister(2)
+    c = ClassicalRegister(2)
     circuit = QuantumCircuit(q, c)
 
     # Quantum circuit starts here
     circuit.h(q[0])
+    circuit.cnot(q[0], q[1])
     circuit.measure(q, c)
     # End quantum circuit
 
